a95fdce316323465e62a9f3a69da4d48a0dd10d4
JGUAL <joaquingual1@gmail.com>
2022-05-10 15:03:38 -0300

    NavBar Added

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+
+export const NavBar = () => {
+    return (
+        <>
+            <Navbar bg="dark" expand="lg" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#inicio">Cereal Paz</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#inicio">Inicio</Nav.Link>
+                            <Nav.Link href="#productos">Productos</Nav.Link>
+                            <Nav.Link href="#promociones">Promociones</Nav.Link>
+                            <Nav.Link href="#Contacto">Contacto</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    )
+}
